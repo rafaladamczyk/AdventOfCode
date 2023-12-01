52b64c0562c5a633351b6b0be0ba9752d8df5658
Rafał Adamczyk <rafal.adamczyk@gmail.com>
2023-12-01 06:32:22 +0100

    day 1

--- ConsoleApp1/2023/Day1.cs
+++ ConsoleApp1/2023/Day1.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AdventOfCode;
 using AdventOfCode.Utils;
 
@@ -9,15 +11,59 @@ namespace AoC2023
         public async Task<object> Part1()
         {
             var input = await Input.GetInput(2023, 1);
-            //var input = await Input.GetExampleInput();
-            return "";
+            var numbers = input.Select(x => (x.First(char.IsDigit), x.Last(char.IsDigit)))
+                .Select(x => int.Parse($"{x.Item1}{x.Item2}"));
+            return numbers.Sum();
         }
 
         public async Task<object> Part2()
         {
+            var digits = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
             var input = await Input.GetInput(2023, 1);
-            //var input = await Input.GetExampleInput();
-            return "";
+            var numbers = new List<int>();
+            foreach (var line in input)
+            {
+                int first = 0;
+                int last = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    var digit = GetDigitAt(line, i);
+                    if (digit != null)
+                    {
+                        if (first == 0)
+                        {
+                            first = digit.Value;
+                        }
+
+                        last = digit.Value;
+                    }
+                }
+
+                var number = int.Parse($"{first}{last}");
+                numbers.Add(number);
+            }
+
+            return numbers.Sum();
+
+            int? GetDigitAt(string s, int i)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    return int.Parse($"{s[i]}");
+                }
+
+                for (int j = 0; j < digits.Length; j++)
+                {
+                    if (s.Substring(i).StartsWith(digits[j]))
+                    {
+                        return j;
+                    }
+
+                }
+
+                return null;
+            }
         }
     }
 }
